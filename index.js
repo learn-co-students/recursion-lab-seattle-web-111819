e70a0239eec056bcaf7193dedc1c47cefa10c9b4
bahayg <bahayg@gmail.com>
2020-11-21 09:44:17 -0800

    Done.

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ printString = string => {
     return  printString(string.substring(1, string.length))
 }
 
-reverseString = string => {
+function reverseString(string) {
     if (string.length === 1) return string
     return string[string.length-1] + reverseString(string.slice(0, -1))
 } 
 
-isPalindrome = string => {
+function isPalindrome(string) {
     if (string.length === 1) return true
     if (string[0] === string[string.length-1]) {
         return isPalindrome(string.slice(1,-1))
     } else return false
 }
 
-addUpTo = (array, index) => {
+function addUpTo(array, index) {
     if (index === 0) return array[0]
     return array[index] + addUpTo(array, --index)
 }
 
-maxOf = array => {
+function maxOf(array) {
     if(array.length < 2) return array
     return Math.max(array.pop(), maxOf(array))
 }
 
-includesNumber = (array, number) => {
+function includesNumber(array, number) {
     if (number === array[0]) return true
     else if (array.length === 0) return false
     else return includesNumber(array.slice(1), number)
